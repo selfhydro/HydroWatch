611b854c4bb261bf1272aa09067d82cd88fb42c2
Baz <boruch11@gmail.com>
2019-09-19 07:59:41 +1000

    check for docker compose

--- go.mod
+++ go.mod
@@ -4,25 +4,50 @@ go 1.12
 
 require (
 	cloud.google.com/go v0.40.0
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/Microsoft/go-winio v0.3.8 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/Sirupsen/logrus v0.0.0-00010101000000-000000000000 // indirect
+	github.com/containerd/containerd v1.2.7 // indirect
+	github.com/containerd/continuity v0.0.0-20190426062206-aaeac12a7ffc // indirect
+	github.com/containerd/fifo v0.0.0-20190226154929-a9fb20d87448 // indirect
+	github.com/coreos/etcd v3.3.13+incompatible // indirect
+	github.com/docker/distribution v0.0.0-20170726174610-edc3ab29cdff // indirect
+	github.com/docker/docker v1.13.1 // indirect
+	github.com/docker/go-connections v0.3.0 // indirect
+	github.com/docker/go-metrics v0.0.0-20181218153428-b84716841b82 // indirect
+	github.com/docker/go-units v0.0.0-20170127094116-9e638d38cf69 // indirect
+	github.com/docker/libcompose v0.4.0
+	github.com/docker/libtrust v0.0.0-20150526203908-9cbd2a1374f4 // indirect
+	github.com/docker/swarmkit v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/gliderlabs/ssh v0.2.2 // indirect
-	github.com/golang/mock v1.3.1 // indirect
-	github.com/google/btree v1.0.0 // indirect
-	github.com/google/pprof v0.0.0-20190515194954-54271f7e092f // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
-	github.com/kr/pty v1.1.5 // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/gorilla/context v0.0.0-20160226214623-1ea25387ff6f // indirect
+	github.com/gorilla/mux v0.0.0-20160317213430-0eeaf8392f5b // indirect
+	github.com/hashicorp/go-memdb v1.0.3 // indirect
+	github.com/mattn/go-shellwords v1.0.5 // indirect
+	github.com/opencontainers/go-digest v0.0.0-20170106003457-a6d0ee40d420 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.0.0-20161109192122-51371867a01c // indirect
+	github.com/opencontainers/runtime-spec v1.0.1 // indirect
+	github.com/opencontainers/selinux v1.2.2 // indirect
+	github.com/prometheus/client_golang v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/vbatts/tar-split v0.11.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20151027082146-e0fe6f683076 // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20150808065054-e02fc20de94c // indirect
+	github.com/xeipuuv/gojsonschema v0.0.0-20160323030313-93e72a773fad // indirect
 	go.opencensus.io v0.22.0 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
 	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4 // indirect
-	golang.org/x/exp v0.0.0-20190510132918-efd6b22b2522 // indirect
-	golang.org/x/image v0.0.0-20190622003408-7e034cad6442 // indirect
-	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88 // indirect
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	golang.org/x/tools v0.0.0-20190627033414-4874f863e654 // indirect
 	google.golang.org/api v0.7.0 // indirect
 	google.golang.org/appengine v1.6.1 // indirect
 	google.golang.org/genproto v0.0.0-20190626174449-989357319d63 // indirect
@@ -30,3 +55,7 @@ require (
 	gopkg.in/src-d/go-git.v4 v4.12.0
 	gopkg.in/yaml.v2 v2.2.2
 )
+
+replace github.com/Sirupsen/logrus => github.com/sirupsen/logrus v1.4.0
+
+replace github.com/docker/docker => github.com/docker/engine v0.0.0-20180718150940-a3ef7e9a9bda
--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"cloud.google.com/go/storage"
+	"github.com/docker/libcompose/docker"
+	"github.com/docker/libcompose/project"
+	"github.com/docker/libcompose/project/options"
 	"go.uber.org/zap"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/yaml.v2"
@@ -26,6 +31,11 @@ type app struct {
 
 var version []byte
 var sugar *zap.SugaredLogger
+
+var NoDockerComposeError = errors.New("repo does not contain a docker compose file")
+var LastMessage = func(format string, a ...interface{}) {
+	fmt.Printf(format, a)
+}
 
 func main() {
 	logger, _ := zap.NewProduction()
@@ -53,13 +63,17 @@ func main() {
 	}
 
 	for applicationName, app := range applications.Apps {
-		newVersion, err := read(client, app.VersionBucket, applicationName)
-		CheckIfError(err)
+		sugar.Infof("checking for latest version of %s", applicationName)
+		messyVersion, err := read(client, app.VersionBucket, applicationName)
+		if err != nil {
+			sugar.Fatalf("can not get latest version b/c %s", err.Error())
+		}
+		newVersion := CleanNewVersion(messyVersion)
 		if string(newVersion) == string(version) {
 			sugar.Infow("no new updates for %s", applicationName)
 			break
 		}
-		gitDirectory := fmt.Sprintf("tmp/%s/", applicationName)
+		gitDirectory := fmt.Sprintf("tmp/%s", applicationName)
 		if _, err := os.Stat(gitDirectory); os.IsNotExist(err) {
 			Info("git clone %s", app.DockerComposeURL)
 			_, err := git.PlainClone(gitDirectory, false, &git.CloneOptions{
@@ -67,14 +81,62 @@ func main() {
 				Progress: os.Stdout,
 			})
 			CheckIfError(err)
+		} else {
+			r, err := git.PlainOpen(gitDirectory)
+			CheckIfError(err)
+			w, err := r.Worktree()
+			CheckIfError(err)
+			Info("git pull origin %s", gitDirectory)
+			err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+			CheckIfError(err)
 		}
 
-		cmdStr := fmt.Sprintf("docker-compose -f %s.yml up -d", gitDirectory)
-		out, err := exec.Command("/bin/sh", "-c", cmdStr).Output()
-		CheckIfError(err)
-		fmt.Printf("%s", out)
-	}
+		dockerFileLocation := fmt.Sprintf("%s/docker-compose.yml", gitDirectory)
+		err = DoesDockerComposeFileExist(dockerFileLocation)
+		if err != nil {
+			sugar.Fatal(err)
+		}
 
+		project, err := docker.NewProject(&ctx.Context{
+			Context: project.Context{
+				ComposeFiles: []string{dockerFileLocation},
+				ProjectName:  applicationName,
+			},
+		}, nil)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = project.Up(context.Background(), options.Up{})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cmdArgs := []string{"-f", dockerFileLocation, "up", "-d"}
+		dockerImageVersionTag := fmt.Sprintf("TAG=%s", string(newVersion))
+		cmd := exec.Command("docker-compose", cmdArgs...)
+		cmd.Env = append(cmd.Env, dockerImageVersionTag)
+		Info("looking for docker image with version %s", string(newVersion))
+		out, err := cmd.Output()
+		if err != nil {
+			sugar.Fatalf(string(err.(*exec.ExitError).Stderr))
+		}
+		CheckIfError(err)
+		Info("%s", string(out))
+	}
+}
+
+func CleanNewVersion(version []byte) string {
+	return strings.TrimSuffix(string(version), "\n")
+}
+
+func DoesDockerComposeFileExist(dockerFileLocation string) error {
+	if _, err := os.Stat(dockerFileLocation); os.IsNotExist(err) {
+		return NoDockerComposeError
+	}
+	return nil
 }
 
 func read(client *storage.Client, bucket, object string) ([]byte, error) {
@@ -93,12 +155,16 @@ func read(client *storage.Client, bucket, object string) ([]byte, error) {
 }
 
 func Info(format string, args ...interface{}) {
-	sugar.Infow("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+	sugar.Infow(fmt.Sprintf(format, args...))
 }
 
 func CheckIfError(err error) {
-	if err == nil {
+	if err == nil || IsNoErrAlreadyUpToDate(err) {
 		return
 	}
 	sugar.Fatalf(err.Error())
+}
+
+func IsNoErrAlreadyUpToDate(err error) bool {
+	return err.Error() == git.NoErrAlreadyUpToDate.Error()
 }
